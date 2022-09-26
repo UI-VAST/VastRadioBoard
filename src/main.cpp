45b635e38b68d7d54a3edc713dee87e31239a64e
rymste <rymste@gmail.com>
2022-09-26 15:46:51 -0700

    added parachute and cutdown code

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@
 #include <SoftwareSerial.h>
 #include "CRC.h"
 
+#define CutDownNichromeTime 5000 //milli sec
+#define CutDownPin 34
+#define ParachutePin 35
+bool nichromeON = false;
+unsigned long CutDownStart = 0;
+
 
 static const uint32_t GPSBaud = 9600;
 TinyGPSPlus gps;
@@ -13,6 +19,9 @@ PacketSerial lora_PacketSerial;
 SoftwareSerial gpsSerial(8, 9);
 HardwareSerial lora(2);
 HardwareSerial rfd(1);
+
+unsigned long MillisCount1 = 0;
+unsigned long MillisCount2 = 0;
 
 typedef struct packet {
   double lat;
@@ -25,7 +34,8 @@ typedef struct packet {
   bool timer_running;
   bool cutdown_status;
   bool parachute_status;
-  int bad_packet;
+  int lora_bad_packet;
+  int rfd_bad_packet;
 } packet;
 packet myPacket;
 
@@ -38,8 +48,29 @@ typedef struct recieved_data{
 } recieved_data;
 recieved_data rx_data;
 
+void parachuteUpdate(){
+  if(myPacket.parachute_status == true){
+      digitalWrite(ParachutePin, HIGH);
+  }
+  else{
+    digitalWrite(ParachutePin, LOW);
+  }
+}
 
-
+void cutdownUpdate(){
+  if(myPacket.cutdown_status == true){
+    if(nichromeON = false){
+      CutdownStart = millis();
+      digitalWrite(CutDownPin, HIGH);
+      nichromeON = true;
+    }
+    if(millis() > (CutDownStart + CutDownNichromeTime)){
+      digitalWrite(CutDownPin, LOW);
+      nichromeON = false;
+      myPacket.cutdown_status = false;
+    }
+  }
+}
 
 void check_for_commands(){
   if(rx_data.trigger_cutdown){
@@ -57,40 +88,44 @@ void check_for_commands(){
   if(rx_data.update_cutdown_time){
     myPacket.cutdown_time = rx_data.cutdown_time;
   }
+  if(myPacket.timer_running == true){
+    myPacket.cutdown_time--;
+  }
+  if(myPacket.cutdown_time < 1){
+    myPacket.cutdown_status = true;
+  }
 }
 
 void read_gps(){
   if(gpsSerial.available() > 0){
-    if(gpsSerial.available() > 0){
-      if(gps.encode(gpsSerial.read())){
-        if(gps.location.isValid()){
-          myPacket.lat = gps.location.lat();
-          myPacket.lng = gps.location.lng();
-        }
-        if(gps.altitude.isValid()){
-          myPacket.alt = gps.altitude.meters();
-        }
-        if(gps.satellites.isValid()){
-          myPacket.sats = gps.satellites.value();
-        }
-        if(gps.speed.isValid()){
-          myPacket.speed = gps.speed.mph();
-        }
+    if(gps.encode(gpsSerial.read())){
+      if(gps.location.isValid()){
+        myPacket.lat = gps.location.lat();
+        myPacket.lng = gps.location.lng();
+      }
+      if(gps.altitude.isValid()){
+        myPacket.alt = gps.altitude.meters();
+      }
+      if(gps.satellites.isValid()){
+        myPacket.sats = gps.satellites.value();
+      }
+      if(gps.speed.isValid()){
+        myPacket.speed = gps.speed.mph();
       }
     }
-  }  
+  }
 }
 
 void rfd_PacketReceived(const uint8_t* buffer, size_t size)
 {
   uint32_t crc1 = CRC::Calculate(buffer, size, CRC::CRC_32());
   uint32_t crc2;
-  memcpy(&crc2, &buffer[size], sizeof(crc2));
+  memcpy(&crc2, &buffer[sizeof(rx_data)], sizeof(crc2));
   if(crc1 == crc2){
     memcpy(&rx_data, &buffer, sizeof(rx_data)); 
   }
   else{
-    myPacket.bad_packet++;
+    myPacket.rfd_bad_packet++;
   }
 }
 
@@ -98,12 +133,12 @@ void lora_PacketReceived(const uint8_t* buffer, size_t size)
 {
   uint32_t crc1 = CRC::Calculate(buffer, size, CRC::CRC_32());
   uint32_t crc2;
-  memcpy(&crc2, &buffer[size], sizeof(crc2));
+  memcpy(&crc2, &buffer[sizeof(rx_data)], sizeof(crc2));
   if(crc1 == crc2){
     memcpy(&rx_data, &buffer, sizeof(rx_data)); 
   }
   else{
-    myPacket.bad_packet++;
+    myPacket.lora_bad_packet++;
   }
 }
 
@@ -127,23 +162,31 @@ void setup() {
   lora_PacketSerial.setStream(&lora);
   lora_PacketSerial.setPacketHandler(&lora_PacketReceived);
 
+  myPacket.cutdown_status = false;
+  myPacket.cutdown_time = 3600;
+  myPacket.parachute_status = false;
 
+  pinMode(CutDownPinm OUTPUT);
+  pinMode(ParachutePin, OUTPUT);
 }
 
-unsigned long MillisCount1 = 0;
-unsigned long MillisCount2 = 0;
- 
 void loop() {
-
+  rfd_PacketSerial.update();
+  lora_PacketSerial.update();
+  cutdownUpdate();
+  parachuteUpdate();
   //run at 10hz
   unsigned long currentMillis = millis();
   if(currentMillis - MillisCount1 >= 100){
     MillisCount1 = currentMillis;
     Send_packet();
+    check_for_commands();
   }
+
   //run at 1hz
   if(currentMillis - MillisCount2 >= 1000){
     MillisCount2 = currentMillis;
+    read_gps();
   }
     
 }
